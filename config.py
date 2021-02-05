3ff673883e85e8ae69d2b97309972e927dabb6a0
aptswati <56195225+aptswati@users.noreply.github.com>
2021-02-05 10:05:56 -0800

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or b'\nr\x81\xf1\x172u\xa7\xcbu\xff\x85\x9cO%\xea'
 
-
-MONGODB_SETTINGS = {'db': 'Kudos_db',
-'host':'mongodb://datastore:27017/Kudos_db'
-                    }
+client = MongoClient('mongodb://datastore:27017/Kudos_db')
+db = client.Kudos_db
+ 
+                   
