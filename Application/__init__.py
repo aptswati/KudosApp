814e932fd62686f2871af38539b76cfd3a278dc4
aptswati <aptswati16@gmail.com>
2021-02-05 05:10:06 -0800

    move includes outside

--- Application/__init__.py
+++ Application/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from config import Config
 from flask_mongoengine import MongoEngine
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.config.from_object(Config)
 
 db = MongoEngine()
