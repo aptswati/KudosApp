f4236913fbf624c3a3ae8172474e3a3fae83ac3b
aptswati <56195225+aptswati@users.noreply.github.com>
2021-02-05 18:05:16 -0800

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,10 @@ from pymongo import MongoClient
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or b'\nr\x81\xf1\x172u\xa7\xcbu\xff\x85\x9cO%\xea'
+    
+   CONNECTION_STRING = "mongodb+srv://aptswati:<password>@cluster0.2fqdn.mongodb.net/<dbname>?retryWrites=true&w=majority"
+client = pymongo.MongoClient(CONNECTION_STRING)
+db = client.get_database('flask_mongodb_atlas')
 
-client = MongoClient('mongodb://datastore:27017/Kudos_db')
-db = client.Kudos_db
  
                    
